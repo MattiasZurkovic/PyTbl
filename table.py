dbf700a6f1b99cee404c0e5b2f132f8ebc119f4d
MattiasZurkovic <mattias@zurkovic.com>
2015-08-15 21:41:40 +0200

    Formating

--- table.py
+++ table.py
@@ -9,9 +9,16 @@ def new(columns):
     print('\r')
     for i in columns:
         stdout.write('|   ' + i + '   |')
+    print('\r')
+    for i in columns:
+        length = len(i)
+        stdout.write('-' * (length + 8))
+    print('\r')
+
 
 def add_row(input):
     for x in input:
         stdout.write('|   ' + x + '   |')
 
 new(['Animal', 'Danger'])
+add_row(['Bear', 'Yes'])
