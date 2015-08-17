8ec57fb7545d039baa22a46d97b89be33d3e5ef8
MattiasZurkovic <mattias@zurkovic.com>
2015-08-17 13:21:17 +0200

    More suto

--- table.py
+++ table.py
@@ -5,6 +5,8 @@ from sys import stdout
 global label_length
 global label_style
 
+
+
 def new_columns(columns, style):
     if style == 1:
         label_style = '='
@@ -27,21 +29,19 @@ def new_columns(columns, style):
     print('\r')
 
     for i in columns:
-        line_pos = ' |'
-        stdout.write('| ' + i + line_pos)
+        line_pos_right = ' |'
+        stdout.write('| ' + i + line_pos_right)
     print('\r')
 
     for i in columns:
-        length = len(i)
         stdout.write('+' + label_style * (length + 2) + '+')
     print('\r')
-
-
 
 
 # Add a row - this method adds a row to the table
 def add_row(input):
     for x in input:
+        input_length = len(x)
         stdout.write('| ' + x + ' |')
     print('\r')
     for x in input:
@@ -51,8 +51,32 @@ def add_row(input):
 
 
 
-new_columns(['Animal', 'Damage Count', 'Name  '], 1)
-add_row(['Bear  ', '2000        ', 'Po    '])
-add_row(['Cat   ', '3           ', 'Mew   '])
-add_row(['Dog   ', '-10         ', 'Hunter'])
+def add_column(column_title, column_data):
+    for m in column_title:
+        length = len(m)
+        stdout.write('+' + '=' * (length + 2) + '+')
+    print('\r')
+
+    for m in column_title:
+        line_pos_right = ' |'
+        stdout.write('| ' + m + ' |')
+    print('\r')
+
+    for m in column_title:
+        stdout.write('+' + '=' * (length + 2) + '+')
+    print('\r')
+# ----------------------------------------------------
+    for m in column_data:
+        this_length = len(m)
+
+        if this_length < length:
+            wanted_l = length - this_length
+        elif this_length > length:
+            wanted_l = this_length - length
+
+        stdout.write('| ' + m + ' ' * (wanted_l) + ' |')
+    print('\r')
+
+
+add_column(['Animal'], ['Hello', 'Cat'])
 
